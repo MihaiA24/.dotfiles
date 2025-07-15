7bd6aa2c7a2cfe658af4758b0bd2669dda56b98b
Mihai Aftinescu <93051145+MihaiA24@users.noreply.github.com>
2025-07-15 23:32:22 +0200

    Added Homebrew forumulas and changed installation order

--- setup_linux.sh
+++ setup_linux.sh
@@ -21,10 +21,6 @@ source ~/.bashrc
 # Install UV python package manager
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-# Install ZSH and OMZ
-sudo apt-get install git zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -34,5 +30,13 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 sudo apt install openjdk-17-jdk
 
 
+# Install ZSH and OMZ
+sudo apt-get install git zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+rm ~/.zshrc # Remove generated zshrc by OMZ
 # Last create sym links to .dotfiles repositores
 ./create_sim_links.sh
+
+brew bundle install --file=./Brewfile
