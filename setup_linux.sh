913f037757983e19c9481eddd6703544b7a0e659
Mihai Aftinescu <93051145+MihaiA24@users.noreply.github.com>
2025-07-20 18:04:26 +0200

    Added p10k custom fonts, docker engine, changed omz for zsh4humans

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,7 +1,7 @@
 # In this scripts go all CMD lines that can be executed from scratch without 0 addional config or manual download
 
 # First add source repositories
-./setup_source_repositories.sh
+sh ./setup_source_repositories.sh
 
 # Install 1Password
 sudo apt update && sudo apt install 1password
@@ -24,19 +24,32 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/home/linuxbrew/.linuxbrew/bin/brew bundle install --file=./Brewfile
 
 
 # Install JAVA
 sudo apt install openjdk-17-jdk
 
 
-# Install ZSH and OMZ
-sudo apt-get install git zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Download first p10k fonts
+curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf --output 'MesloGS NF Regular.ttf'
+curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf --output 'MesloGS NF Bold.ttf'
+curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf --output 'MesloGS NF Italic.ttf'
+curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf --output 'MesloGS NF Bold Italic.ttf'
+
+mkdir -p ~/.local/share/fonts
+mv *.ttf ~/.local/share/fonts
+fc-cache -f -v # Refresh font cache
 
 
 rm ~/.zshrc # Remove generated zshrc by OMZ
+rm ~/.p10k.zsh # Remove generated zshrc by OMZ
 # Last create sym links to .dotfiles repositores
 ./create_sim_links.sh
 
-brew bundle install --file=./Brewfile
+
+
+# Install Docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker ${USER}
