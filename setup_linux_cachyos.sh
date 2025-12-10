9966b98c96a76a79cf00def27817ca965297dc85
Mihai Aftinescu <git.rule370@passmail.net>
2025-12-10 21:02:41 +0100

    (feat, refactor) Reorganized by groups and added setup paru packages

--- setup_linux_cachyos.sh
+++ setup_linux_cachyos.sh
@@ -2,25 +2,39 @@
 
 sudo paru # Update all installed
 
+
+# Code tools
 paru -S zed
-paru -S 1password
 paru -S visual-studio-code-bin # alternative paru -S code # This is the open source version
 paru -S jetbrains-toolbox
-paru -S paru -S jdk21-openjdk # The openjdk is availiable in cachy os ryzen repo
+paru -S dbeaver # Open source database management tool
+paru -S bruno # Postman alternative
 paru -S ghostty # Terminal
-paru -S rustup
-rustup default stable # Install rust stable version
-paru -S miniconda3
-paru -S bruno
-paru -S telegram-desktop
-paru -S megasync-bin
-paru -S qbittorrent
-paru -S dbeaver
-paru -S zip
 
+# Programming languages software
+paru -S uv # Package manager for python
+# paru -S miniconda3 # Using uv by default
+paru -S paru -S jdk21-openjdk # The openjdk is availiable in cachy os ryzen repo
+
+# Other programs
+paru -S telegram-desktop 
+paru -S megasync-bin # Mega sync cloud
+paru -S qbittorrent # Open source torrent client
+paru -S discord 
+paru -S google-chrome # Chrome browser
+paru -S brave-bin # Brave browser
+paru -S anytype-bin # P2P note taking app
+paru -S vlc vlc-plugins-all
+paru -S mission-center # Windows like System Monitor
+paru -S todoist-appimage # Daily planner app
+
+
+## Command line tools
 paru -S btop # Modern top
 paru -S nvtop # Modern top for nvidia GPU
 paru -S ncdu # Modern du
+paru -S zip 
+paru -S neovim
 # Already installed package by default installation in CachyOS
 # paru -S exa # Modern ls
 # paru -S bat # Modern cat
@@ -34,11 +48,8 @@ paru -S proton-pass
 paru -S proton-vpn-gtk-app # Official Proton VPN app, available in CachyOS Repo
 
 
-paru -S vlc
-paru -S mission-center # Windows like System Monitor
-
 # Docker
-paru -S docker
+paru -S docker docker-buildx docker-compose
 paru -S nvidia-container-toolkit # Use docker with GPU support
 sudo systemctl start docker
 sudo systemctl enable docker
